be7238be8b4c43579093e2923f5617a54f111ab8
sarthak ganure <sarthakganure@sarthaks-MacBook-Air.local>
2024-06-10 22:40:00 +0530

    footer init

--- app/page.js
+++ app/page.js
@@ -3,12 +3,14 @@ import Image from "next/image";
 import Navbar from "@/components/Navbar";
 import Hero_section from "@/components/Hero_section";
 import What_we_do from "@/components/What_we_do";
+import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <>
       <Navbar/>
       <Hero_section/>
       <What_we_do/>
+      <Footer/>
     </>
   );
 }
