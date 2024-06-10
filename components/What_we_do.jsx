95d69f530ece0f6d27817ddaa0934f2456a59d01
sarthak ganure <sarthakganure@sarthaks-MacBook-Air.local>
2024-06-10 17:27:29 +0530

    home page nearly finished

--- components/What_we_do.jsx
+++ components/What_we_do.jsx
@@ -1,22 +1,189 @@
 import React from "react";
-
+import Image from "next/image";
+import { Zoom } from "react-awesome-reveal";
+import styles from "./What_we_do.module.css";
 const What_we_do = () => {
   return (
-    <div className="p-6 md:p-12 bg-gray-100 text-gray-900">
-      <h1 className="text-3xl md:text-5xl font-bold mb-6 text-center">What We Do</h1>
-      <p className="text-lg md:text-xl leading-relaxed text-center justify-center">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum nemo ut
-        fugiat facilis? Ullam, dolorum? Repellendus reiciendis inventore quam
-        quod molestias voluptate minus, laborum culpa. Facere officia et
-        architecto, ut quo nemo, eaque magnam alias, iste recusandae at ipsum
-        reprehenderit quibusdam est perspiciatis neque vitae sit! Sed at vel
-        praesentium ab neque debitis dolore vitae, animi facere impedit
-        consequatur tempora reiciendis similique est assumenda repellendus
-        possimus alias, sequi corporis quas, architecto provident quaerat hic
-        voluptatem! Dolor tempora animi assumenda quos? Esse est explicabo
-        tempore, iste autem quae modi doloremque, quaerat obcaecati a vel labore
-        adipisci illum non deserunt amet minus?
+    <div className="p-6 md:p-12 bg-black text-gray-100">
+      <h1 className="text-3xl md:text-5xl font-bold mb-6 text-center">
+        What We Do
+      </h1>
+      <p className="text-lg md:text-xl leading-relaxed text-center">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident
+        fugiat earum eaque optio in nam distinctio hic, aperiam magnam laborum
+        similique maiores commodi rem saepe placeat harum dolor deserunt
+        explicabo deleniti laudantium perferendis ea ipsam, animi mollitia.
+        Alias maxime quod esse! Totam, deleniti! Perspiciatis eveniet nobis
+        labore et nesciunt, ipsam quisquam distinctio nisi dolorem enim neque
+        unde. Deserunt nemo iusto eveniet impedit earum, minus, dolor provident
+        est ullam eum hic, dolorem vero sit porro. A recusandae reprehenderit
+        sunt itaque at molestiae minima laboriosam facilis perferendis placeat
+        error corporis porro ducimus voluptas quod, provident molestias esse
+        voluptatibus aliquam culpa eius rerum!
       </p>
+      <Zoom>
+        <div className="flex justify-center items-center mt-5">
+          <Image src="/whatwedo.png" width={1000} height={1000} />
+        </div>
+      </Zoom>
+      <hr className="my-20 border-gray-300" />{" "}
+      {/* Add a horizontal line with margin */}
+      <div className="container mx-auto px-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+          <div className="text-center md:text-left">
+            <h2 className="text-2xl font-bold mb-4 text-orange-600">
+              AI Consulting and Strategy
+            </h2>
+            <h1 className="text-4xl font-bold mb-4 text-white">
+              Navigate AI's complexities with a strategic compass, transforming
+              vision into value
+            </h1>
+          </div>
+          <div className="text-center md:text-left">
+            <p className="mb-4 bg-orange-700 text-white p-4 rounded-lg relative pl-8">
+              <span className="absolute h-4 w-4 bg-black rounded-lg left-2 top-1/2 transform -translate-y-1/2"></span>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+            <p className="mb-4 bg-orange-700 text-white p-4 rounded-lg relative pl-8">
+              <span className="absolute h-4 w-4 bg-black rounded-lg left-2 top-1/2 transform -translate-y-1/2"></span>
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
+        </div>
+      </div>
+      <hr className="my-20 border-gray-300" />{" "}
+      {/* Add a horizontal line with margin */}
+      <div className="container mx-auto px-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+          <div className="text-center md:text-left">
+            <h2 className="text-2xl font-bold mb-4 text-orange-600">
+              AI Software Development
+            </h2>
+            <h1 className="text-4xl font-bold mb-4 text-white">
+              Elevate your operations with bespoke AI-driven software solutions.
+            </h1>
+          </div>
+          <div className="text-center md:text-left">
+            <p className="mb-4 bg-orange-700 text-white p-4 rounded-lg relative pl-8">
+              <span className="absolute h-4 w-4 bg-black rounded-lg left-2 top-1/2 transform -translate-y-1/2"></span>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+            <p className="mb-4 bg-orange-700 text-white p-4 rounded-lg relative pl-8">
+              <span className="absolute h-4 w-4 bg-black rounded-lg left-2 top-1/2 transform -translate-y-1/2"></span>
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
+        </div>
+      </div>
+      <hr className="my-20 border-gray-300" /> {/* Additional hr tag */}
+      {/* Add a horizontal line with margin */}
+      <div className="container mx-auto px-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+          <div className="text-center md:text-left">
+            <h2 className="text-2xl font-bold mb-4 text-orange-600">
+              Data and Machine Learning Engineering
+            </h2>
+            <h1 className="text-4xl font-bold mb-4 text-white">
+              Transform your data into actionable insights with tailored machine
+              learning techniques.
+            </h1>
+          </div>
+          <div className="text-center md:text-left">
+            <p className="mb-4 bg-orange-700 text-white p-4 rounded-lg relative pl-8">
+              <span className="absolute h-4 w-4 bg-black rounded-lg left-2 top-1/2 transform -translate-y-1/2"></span>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+            <p className="mb-4 bg-orange-700 text-white p-4 rounded-lg relative pl-8">
+              <span className="absolute h-4 w-4 bg-black rounded-lg left-2 top-1/2 transform -translate-y-1/2"></span>
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
+        </div>
+      </div>
+      <hr className="my-20 border-gray-300" /> {/* Additional hr tag */}
+      <div className="container mx-auto px-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+          <div className="text-center md:text-left">
+            <h1 className="text-3xl mb-6 uppercase">
+              Why Companies Work with Us
+            </h1>
+            <p className="mb-6 text-sm">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
+              ipsum, dolor sit amet consectetur adipisicing elit. Est,
+              veritatis.
+            </p>
+            <ul className={`${styles["list-square-orange"]} mb-8`}>
+              <li className="mb-4">
+                Development of your product as an end-to-end solution, outsource
+                / outstaff / in-house cooperation
+              </li>
+              <li className="mb-4">
+                Development of your product as an end-to-end solution, outsource
+                / outstaff / in-house cooperation
+              </li>
+              <li className="mb-4">
+                Development of your product as an end-to-end solution, outsource
+                / outstaff / in-house cooperation
+              </li>
+              <li className="mb-4">
+                Development of your product as an end-to-end solution, outsource
+                / outstaff / in-house cooperation
+              </li>
+            </ul>
+            <button className="bg-orange-600 text-white px-4 py-2 rounded-lg mb-8">
+              Discuss Your Project
+            </button>
+          </div>
+
+          <div
+            className="relative text-center md:text-left justify-center flex items-center"
+            style={{
+              position: "relative",
+            }}
+          >
+            <Image
+              src="/orange-circle-1462825_1920.png"
+              height={500}
+              width={500}
+              alt="Image"
+              className="w-64 h-64 object-cover rounded-lg"
+            />
+            <div className="absolute z-10 text-white font-bold text-4xl">
+              <p className="text-center items-center">98%</p>
+              <p className="text-xs mt-2 uppercase">High Performance</p>
+            </div>
+            <div
+              style={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                width: "50%",
+                height: "3px",
+                backgroundColor: "white",
+                zIndex: 5,
+              }}
+            ></div>
+            <div
+              style={{
+                position: "absolute",
+                bottom: 0,
+                right: 0,
+                width: "50%",
+                height: "3px",
+                backgroundColor: "white",
+                zIndex: 5,
+              }}
+            ></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
